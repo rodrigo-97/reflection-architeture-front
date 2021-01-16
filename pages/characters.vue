0b0d61872f308caf58a8fa8e6801f4b2325168b0
rodrigo matheus <rodmatheus99@gmail.com>
2021-01-15 23:52:42 -0300

    simples front almos finished

---
+++ pages/characters.vue
@@ -0,0 +1,32 @@
+<template>
+  <transition name="home">
+    <div class="section dark">
+      <div class="columns">
+        <div class="column is-6">
+          <table style="width: 100%">
+            <tr>
+              <th>#</th>
+              <th>Nome</th>
+              <th>Nº de ep.</th>
+              <th>Ações</th>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Attack on Titan</td>
+              <td>60</td>
+              <td>
+                <b-button type="is-danger">Excluir</b-button>
+                <b-button type="is-primary">Alterar</b-button>
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Naruto</td>
+              <td>500</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
